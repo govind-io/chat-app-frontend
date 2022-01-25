65703df6ddec5cfbeafa469fd7137062ad3792aa
govindgovind852 <govindgovind852@gmail.com>
2022-01-25 17:55:21 +0530

    socket connection url updated

--- src/Utils/Connection/SocketConnection.js
+++ src/Utils/Connection/SocketConnection.js
@@ -1,11 +1,14 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
-  "force new connection": true,
-  reconnectionAttempts: "Infinity",
-  timeout: 10000,
-  transports: ["websocket"],
-});
+export const socket = io(
+  "https://git.heroku.com/chat-app-backend-mindrops.git",
+  {
+    "force new connection": true,
+    reconnectionAttempts: "Infinity",
+    timeout: 10000,
+    transports: ["websocket"],
+  }
+);
 
 socket.on("connect", () => {
   console.log("new socket is connected");
